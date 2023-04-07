4b7ecd632f4b657b63fc72ad6b6a2126494b91a8
mrfakename <me@mrfake.name>
2023-04-06 17:34:04 -0700

    Update train.py

--- train.py
+++ train.py
@@ -1,74 +1,63 @@
-import os
-import torch
+from transformers import GPTNeoForCausalLM, GPT2Tokenizer
 from torch.utils.data import Dataset, DataLoader
-from transformers import (
-    AutoTokenizer, 
-    AutoModelForSequenceClassification, 
-    AdamW, 
-    get_linear_schedule_with_warmup,
-)
+import torch
 from tqdm import tqdm
 
-# Set up device
-device = torch.device("cuda" if torch.cuda.is_available() else "mps")
-
-# Set up tokenizer and model
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/pythia-12b")
-model = AutoModelForSequenceClassification.from_pretrained("EleutherAI/pythia-12b")
-model.to(device)
-
-# Set up optimizer and scheduler
-optimizer = AdamW(model.parameters(), lr=5e-5, eps=1e-8)
-scheduler = get_linear_schedule_with_warmup(
-    optimizer, num_warmup_steps=0, num_training_steps=1000
-)
-
-# Define custom dataset class
 class TextDataset(Dataset):
-    def __init__(self, data_dir):
-        self.examples = []
-        for file_name in os.listdir(data_dir):
-            if file_name.endswith(".txt"):
-                with open(os.path.join(data_dir, file_name), "r") as f:
-                    text = f.read()
-                    self.examples.append(text)
+    def __init__(self, directory_path, tokenizer):
+        self.tokenizer = tokenizer
+        self.files = []
+        for root, _, files in os.walk(directory_path):
+            for file in files:
+                self.files.append(os.path.join(root, file))
 
     def __len__(self):
-        return len(self.examples)
+        return len(self.files)
 
     def __getitem__(self, idx):
-        return self.examples[idx]
+        with open(self.files[idx], 'r') as f:
+            text = f.read()
+        encoded_text = self.tokenizer.encode(text, add_special_tokens=False)
+        input_ids = []
+        labels = []
+        for i in range(0, len(encoded_text), self.tokenizer.max_len_single_sentence):
+            input_chunk = encoded_text[i:i+self.tokenizer.max_len_single_sentence]
+            label_chunk = encoded_text[i+1:i+self.tokenizer.max_len_single_sentence+1]
+            input_chunk += [self.tokenizer.pad_token_id] * (self.tokenizer.max_len_single_sentence - len(input_chunk))
+            label_chunk += [self.tokenizer.pad_token_id] * (self.tokenizer.max_len_single_sentence - len(label_chunk))
+            input_ids.append(torch.tensor(input_chunk, dtype=torch.long))
+            labels.append(torch.tensor(label_chunk, dtype=torch.long))
+        return torch.stack(input_ids), torch.stack(labels)
 
-# Define training function
-def train(model, optimizer, scheduler, dataloader, device):
-    model.train()
-    total_loss = 0.0
-    for batch in tqdm(dataloader, desc="Training", leave=False):
-        inputs = tokenizer(batch, padding=True, truncation=True, return_tensors="pt")
-        inputs = inputs.to(device)
-        labels = torch.ones(inputs["input_ids"].shape[0], dtype=torch.long).to(device)
-        optimizer.zero_grad()
-        outputs = model(**inputs, labels=labels)
-        loss = outputs.loss
-        total_loss += loss.item()
+# Initialize the tokenizer and model
+tokenizer = GPT2Tokenizer.from_pretrained('EleutherAI/gpt-neo-2.7B')
+model = GPTNeoForCausalLM.from_pretrained('EleutherAI/gpt-neo-2.7B')
+
+# Use CUDA if available
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+# Initialize the dataset and data loader
+dataset = TextDataset('data', tokenizer)
+dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+
+# Set up the optimizer and scheduler
+optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3)
+
+# Fine-tune the model
+for epoch in range(10):
+    losses = []
+    for input_ids, labels in tqdm(dataloader):
+        input_ids = input_ids.to(device)
+        labels = labels.to(device)
+        loss, _, _ = model(input_ids, labels=labels)
         loss.backward()
         optimizer.step()
-        scheduler.step()
-    return total_loss / len(dataloader)
+        scheduler.step(loss)
+        optimizer.zero_grad()
+        losses.append(loss.item())
+    print(f'Epoch {epoch+1}, loss: {sum(losses)/len(losses)}')
 
-# Define main function
-def main():
-    # Set up dataset and dataloader
-    dataset = TextDataset("data")
-    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
-
-    # Train model
-    for epoch in range(3):
-        loss = train(model, optimizer, scheduler, dataloader, device)
-        print(f"Epoch {epoch+1} loss: {loss:.4f}")
-
-    # Save model
-    torch.save(model.state_dict(), "saved_model/clearmindbeta.pt")
-
-if __name__ == "__main__":
-    main()
+# Save the model
+model.save_pretrained('gpt-j-6b-finetuned')
