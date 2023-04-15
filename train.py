4f1808f5a46279c2b090f77695bd09dc25c2fc75
mrfakename <me@mrfake.name>
2023-04-15 14:47:19 -0700

    Update train.py

--- train.py
+++ train.py
@@ -2,18 +2,20 @@ import pandas as pd
 import torch
 from torch.utils.data import Dataset, random_split
 from transformers import GPT2Tokenizer, TrainingArguments, Trainer, GPTNeoForCausalLM
+#from transformers import GPTNeoXTokenizer, TrainingArguments, Trainer, GPTNeoForCausalLM
 import glob
 
 torch.manual_seed(42)
-tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B", bos_token='<|startoftext|>',
+tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-125m", bos_token='<|startoftext|>',
+#tokenizer = GPTNeoXTokenizer.from_pretrained("EleutherAI/pythia-410m", bos_token='<|startoftext|>',
                                           eos_token='<|endoftext|>', pad_token='<|pad|>')
-model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
+model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-125m")
 model.resize_token_embeddings(len(tokenizer))
 #files = pd.read_csv('data.csv')['description']
 print('Hello World')
 
 path = 'data/*.txt'  # specify path to all .txt files in the data directory
-files = glob.glob(path)[:500]  # get a list of file paths
+files = glob.glob(path)[:50]  # get a list of file paths
 
 contents = []  # initialize an empty list to store contents
 
@@ -50,7 +52,7 @@ class TextDataset(Dataset):
 dataset = TextDataset(contents, tokenizer, max_length=max_length)
 train_size = int(0.9 * len(dataset))
 train_dataset, val_dataset = random_split(dataset, [train_size, len(dataset) - train_size])
-training_args = TrainingArguments(output_dir='./results', num_train_epochs=5, logging_steps=5000, save_steps=5000, per_device_train_batch_size=2, per_device_eval_batch_size=2, warmup_steps=100, weight_decay=0.01, logging_dir='./logs')
+training_args = TrainingArguments(output_dir='./results', num_train_epochs=5, logging_steps=5000, save_steps=5000, per_device_train_batch_size=2, per_device_eval_batch_size=2, warmup_steps=10, weight_decay=0.01, logging_dir='./logs')
 print("Training")
 Trainer(model=model.to(torch.device('mps')), args=training_args, train_dataset=train_dataset,
         eval_dataset=val_dataset, data_collator=lambda data: {'input_ids': torch.stack([f[0] for f in data]), 'attention_mask': torch.stack([f[1] for f in data]).to(torch.device('mps')), 'labels': torch.stack([f[0] for f in data]).to(torch.device('mps'))}).train()
